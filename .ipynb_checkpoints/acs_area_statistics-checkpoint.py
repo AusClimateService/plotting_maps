b3208a00c925bc1cc963bd274069184a624f3603
Gen Tolhurst <gen.tolhurst@bom.gov.au>
2024-08-13 16:44:04 +1000

    update

--- .ipynb_checkpoints/acs_area_statistics-checkpoint.py
+++ .ipynb_checkpoints/acs_area_statistics-checkpoint.py
@@ -1,7 +1,7 @@
 """ Cookie cutter - ACS area statistics
 Use this module to produce summary statistics for Hazards over NCRA regions,
 with the flexibility to apply the method to any shapefile region.
-Typical statistics include median, mean, min, max, 10th, 90th percentiles
+Typical statistics include median, mean, min, max, 10th, 90th percentiles.
 
 This method has used guidance from
 [https://github.com/aus-ref-clim-data-nci/shapefiles/blob/master/python_tutorial.ipynb]"""
@@ -11,32 +11,66 @@ import geopandas as gpd
 import regionmask
 import numpy as np
 import pandas as pd
+from glob import glob
+
+shapefile_list = ["aus_local_gov",
+                  "aus_states_territories",
+                  "australia", 
+                  "broadacre_regions", 
+                  "ncra_regions", 
+                  "NCRA_regions_coastal_waters_GDA94", 
+                  "nrm_regions", ]
+
+name_dict = {"aus_local_gov":"LGA_NAME22", 
+             "aus_states_territories":"STE_NAME21",
+             "australia": "AUS_NAME21",
+             "broadacre_regions": "name",
+             "ncra_regions": "regionname", 
+             "NCRA_regions_coastal_waters_GDA94": "regionname",
+             "nrm_regions":"SubClusNm",
+            }
+
+
+abbr_dict = {"aus_local_gov":"LGA_CODE22", 
+             "aus_states_territories":"ABBREV",
+             "australia": "AUS_CODE21",
+             "broadacre_regions": "aagis",
+             "ncra_regions": "short_name", 
+             "NCRA_regions_coastal_waters_GDA94": "NCRA",
+             "nrm_regions": "SubClusAb",
+            }
+
+def get_regions(shapefiles):
+    """
+    This function takes a list of names of shape files from ia39 and
+    returns a combined regionmask.
+    
+    Parameters
+    -----------
+    name: str
+        one of "aus_local_gov", "aus_states_territories", "australia", 
+        "nrm_regions", "ncra_regions","broadacre_regions",
+        "NCRA_regions_coastal_waters_GDA94"
+
+    Returns
+    -------
+    geopandas dataframe
+    
+    """
+    gdfs = {}
+    PATH = "/g/data/ia39/aus-ref-clim-data-nci/shapefiles/data"
+    
+    for i, shapefile in enumerate(shapefiles):
+        gdfs[i] = gpd.read_file(glob(f"{PATH}/{shapefile}/*.shp")[0]).rename(columns = {name_dict[shapefile]:"NAME", abbr_dict[shapefile]:"abbrevs"}).to_crs(crs = "GDA2020")
+    gdf = pd.concat(gdfs)
+    gdf.index = np.arange(0, len(gdf))
+    return regionmask.from_geopandas(gdf, names="NAME", abbrevs="abbrevs", name= "-".join(shapefiles), overlap=True) 
+
+
 
 # read in the shapefile with regions you will use Australia and NCRA regions
 # from acs_plotting_maps import regions_dict
-ncra_gdf = gpd.read_file(
-    "/g/data/ia39/aus-ref-clim-data-nci/shapefiles/data/ncra_regions/ncra_regions.shp"
-)
-ncra_gdf["abbrevs"] = ncra_gdf["short_name"]
-ncra_gdf["NAME"] = ncra_gdf["regionname"]
-
-aus_gdf = gpd.read_file(
-    "/g/data/ia39/aus-ref-clim-data-nci/shapefiles/data/australia/australia.shp"
-)
-aus_gdf["abbrevs"] = ["AUS"]
-aus_gdf["NAME"] = ["Australia"]
-
-# make sure the projections are the same
-ncra_gdf = ncra_gdf.to_crs(aus_gdf.crs)
-
-gdf = pd.concat(
-    [
-        ncra_gdf[["NAME", "abbrevs", "geometry"]],
-        aus_gdf[["NAME", "abbrevs", "geometry"]],
-    ],
-)
-gdf.index = np.arange(0, 10)
-regions = regionmask.from_geopandas(gdf, names="NAME", abbrevs="abbrevs", overlap=True)
+# regions = get_regions(["ncra_regions", "australia"])
 
 
 def acs_regional_stats(
@@ -44,6 +78,7 @@ def acs_regional_stats(
     infile=None,
     var=None,
     mask=None,
+    regions=get_regions(["ncra_regions", "australia"]),
     start=None,
     end=None,
     dims=None,
@@ -61,6 +96,7 @@ def acs_regional_stats(
     then selects the time range between two years (start and end),
     and applies regions.mask_3D_frac_approx fractional mask (frac)
     to compute a regional statistic (how, eg "mean") over two or three dimensions.
+    Best used with numerical data without nans.
 
     Parameters
     ----------
@@ -100,7 +136,7 @@ def acs_regional_stats(
 
     how: list of str
         List of statistics to reduce data. 
-        List of ['mean', 'median', 'min', 'max', 'sum', 'std', 'var', 'p10', 'p90']. 
+        List of ['mean', 'median', 'min', 'max', 'mode', 'sum', 'std', 'var', 'proportions', 'p10', 'p90', ]. 
         (any pxx where xx is between 0 and 100)
 
     quantile: float [0.0,1.], optional
@@ -119,6 +155,14 @@ def acs_regional_stats(
         If mask =  "min_overlap", you must provide an overlap_threshold value.
         If mask != "min_overlap", this value will be ignored.
 
+    bins: int, sequence of scalars, or IntervalIndex
+        For categorical data. The criteria to bin by using pandas.cut 'bins'
+
+    bin_labels: array or False, default None
+        For labeled categorical data.
+        passed to pandas.cut 'labels'
+        Specifies the labels for the returned bins. Must be the same length as the resulting bins. 
+
     Returns
     -------
     An xarray.DataArray (1D or 2D) with dimension called "region" and values of regional statistics.
@@ -136,10 +180,8 @@ def acs_regional_stats(
     da_mean = acs_regional_stats(ds=ds, 
                                  var="pr",
                                  mask=mask_frac,
-                                 start="1991",
-                                 end="2010", 
-                                 dims=("time", "lat", "lon"),
-                                 how="mean")
+                                 dims=("lat", "lon"),
+                                 how=["mean"])
     """
     # Open file if ds not provided and infile is provided
     if ds is None and infile is not None:
@@ -222,8 +264,6 @@ def acs_regional_stats(
     lat_weights = lat_weights.drop_vars(redundant_coords)
     mask = mask.drop_vars(redundant_coords)
 
-    ds_masked = ds.where(mask)[var]
-
     # create your weighted 3D xr.Dataset
     if var is None:
         try:
@@ -235,6 +275,8 @@ def acs_regional_stats(
         except:
             print(f"Please enter var. One of {list(ds.keys())}")
             return None
+
+    ds_masked = ds.where(mask)[var]
     ds_weighted = ds[var].weighted(mask * lat_weights)
 
     # if calculating stats over all dims:
@@ -248,21 +290,21 @@ def acs_regional_stats(
         if stat.replace("p", "").isnumeric() and (int(stat.replace("p", "")) <= 100):
             q = int(stat.replace("p", "")) / 100
             summary_list.append(
-                ds_weighted.quantile(q, dim=dims, skipna = True)
+                ds_weighted.quantile(q, dim=dims, )
                 .drop_vars("quantile")
                 .rename(f"{var}_{stat}")
             )
         elif stat == "mean":
-            summary_list.append(ds_weighted.mean(dim=dims, skipna = True).rename(f"{var}_{stat}"))
+            summary_list.append(ds_weighted.mean(dim=dims,).rename(f"{var}_{stat}"))
         elif stat == "sum":
-            summary_list.append(ds_weighted.sum(dim=dims, skipna = True).rename(f"{var}_{stat}"))
+            summary_list.append(ds_weighted.sum(dim=dims,).rename(f"{var}_{stat}"))
         elif stat == "std":
-            summary_list.append(ds_weighted.std(dim=dims, skipna = True).rename(f"{var}_{stat}"))
+            summary_list.append(ds_weighted.std(dim=dims,).rename(f"{var}_{stat}"))
         elif stat == "var":
-            summary_list.append(ds_weighted.var(dim=dims, skipna = True).rename(f"{var}_{stat}"))
+            summary_list.append(ds_weighted.var(dim=dims,).rename(f"{var}_{stat}"))
         elif stat == "min":
             summary_list.append(
-                ds_masked.groupby("region").min(dim=dims, skipna = True).rename(f"{var}_{stat}")
+                ds_masked.groupby("region").min(dim=dims,).rename(f"{var}_{stat}")
             )
             if bins is not None:
                 df_masked = ds_masked.to_dataframe()
@@ -275,13 +317,13 @@ def acs_regional_stats(
                 )
         elif stat == "median":
             summary_list.append(
-                ds_weighted.quantile(0.5, dim=dims, skipna=True)
+                ds_weighted.quantile(0.5, dim=dims,)
                 .drop_vars("quantile")
                 .rename(f"{var}_{stat}")
             )
         elif stat == "max":
             summary_list.append(
-                ds_masked.groupby("region").max(dim=dims, skipna = True).rename(f"{var}_{stat}")
+                ds_masked.groupby("region").max(dim=dims,).rename(f"{var}_{stat}")
             )
             if bins is not None:
                 df_masked = ds_masked.to_dataframe()
@@ -311,16 +353,56 @@ def acs_regional_stats(
                     .to_xarray()
                     .rename(f"{var}_{stat}")
                 )
+        elif stat == "proportions":
+            if bins is not None:
+                df_masked = ds_masked.to_dataframe()
+                df_masked["category"] = pd.cut(df_masked[var], bins, labels=bin_labels, ordered=True)
+                proportion_dict = {}
+                props = df_masked.groupby("region").value_counts(["category"], normalize=True).round(4)
+                for i in range(len(ds_masked.region)):
+                    proportion_dict[i] = props[i].to_dict() 
+                    # limit to first 20 categories
+                    proportion_dict[i]  = dict(list(proportion_dict[i].items())[0: 20]) 
+                    
+                d = {"coords": {
+                        "region": {"dims": "region", "data": list(proportion_dict.keys()),}
+                    },
+                     "dims": "region",
+                    "data":list(proportion_dict.values()),
+                    "name": stat}
+                summary_list.append(
+                    xr.DataArray.from_dict(d).rename(f"{var}_cat_{stat}")
+                )
+            else:
+                df_masked = ds_masked.to_dataframe()
+                proportion_dict = {}
+                props = df_masked.groupby("region").value_counts([var], normalize=True).round(4)
+                for i in range(len(ds_masked.region)):
+                    proportion_dict[i] = props[i].to_dict() 
+                    # limit to first 20 categories
+                    proportion_dict[i]  = dict(list(proportion_dict[i].items())[0: 20]) 
+                    
+                d = {"coords": {
+                        "region": {"dims": "region", "data": list(proportion_dict.keys()),}
+                    },
+                     "dims": "region",
+                    "data":list(proportion_dict.values()),
+                    "name": stat}
+                summary_list.append(
+                    xr.DataArray.from_dict(d).rename(f"{var}_{stat}")
+                )
+
         else:
             print(
                 f"{stat} statistic not calculated. \
 Please provide valid how as a list including, one of: \
-['mean', 'median', 'min', 'max','mode', 'sum', 'std', 'var', 'p10', 'p90']"
+['mean', 'median', 'min', 'max', 'mode', 'sum', 'std', 'var', 'p10', 'p90', 'proportions']"
             )
     ds_summary = xr.merge(summary_list)
     df = ds_summary.to_dataframe()
     # drop columns with constant value
-    df_summary = df[[col for col in df.columns if df[col].nunique() != 1]]
+    # df_summary = df[[col for col in df.columns if df[col].nunique() != 1]]
+    df_summary = df
 
     if (
         outfile is None
